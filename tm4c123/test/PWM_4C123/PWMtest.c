7e94ef5332c7c448b58d923f6aff019292a16472
Devangshu Goswami <devgos4@gmail.com>
2021-11-02 12:26:28 -0500

    Added basic servo drivers

---
+++ tm4c123/test/PWM_4C123/PWMtest.c
@@ -0,0 +1,48 @@
+// PWMtest.c
+// Runs on TM4C123
+// Use PWM0/PB6 and PWM1/PB7 to generate pulse-width modulated outputs.
+// Daniel Valvano
+// March 28, 2014
+
+/* This example accompanies the book
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2015
+   Program 6.8, section 6.3.2
+
+   "Embedded Systems: Real-Time Operating Systems for ARM Cortex M Microcontrollers",
+   ISBN: 978-1466468863, Jonathan Valvano, copyright (c) 2015
+   Program 8.4, Section 8.3
+
+ Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+#include <stdint.h>
+#include "../../inc/PLL.h"
+#include "Servo.h"
+
+void WaitForInterrupt(void);  // low power mode
+
+int main(void){
+  PLL_Init(Bus80MHz);               // bus clock at 80 MHz
+  PWM0A_Init(25000, 2500);          // 2 ms duty
+  PWM1A_Init(25000, 1250);
+  PWM1B_Init(25000, 1600);
+  PWM0B_Init(25000, 1875);         // 1.5 ms duty
+  PWM2A_Init(25000, 2200);
+
+  while(1){
+    //WaitForInterrupt();
+      PWM0A_Duty(1250);
+      for(uint32_t i = 0; i < 10000000; i++){};
+      PWM0A_Duty(2500);
+      for(uint32_t i = 0; i < 10000000; i++){};
+  }
+}
---
+++ tm4c123/test/PWM_4C123/Servo.h
@@ -0,0 +1,17 @@
+void PWM0A_Init(uint16_t period, uint16_t duty);
+
+void PWM0A_Duty(uint16_t duty);
+
+void PWM0B_Init(uint16_t period, uint16_t duty);
+
+void PWM0B_Duty(uint16_t duty);
+
+void PWM0G_Init(uint16_t period, uint16_t duty);
+
+void PWM0G_Duty(uint16_t duty);
+
+void PWM1A_Init(uint16_t period, uint16_t duty);
+
+void PWM1B_Init(uint16_t period, uint16_t duty);
+
+void PWM2A_Init(uint16_t period, uint16_t duty);
