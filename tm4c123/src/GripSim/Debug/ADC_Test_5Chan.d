41e26a72f65439129b29123f8dc690e38ba066ff
anuvgupta <me@anuv.me>
2021-11-01 22:10:05 -0500

    Revert "remove extraneous files"
    This reverts commit e9f1ebd90b6080733d336570903871a2b713bb71.

---
+++ tm4c123/src/GripSim/Debug/ADC_Test_5Chan.d
@@ -0,0 +1,51 @@
+# FIXED
+
+ADC_Test_5Chan.obj: ../ADC_Test_5Chan.c
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+ADC_Test_5Chan.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+ADC_Test_5Chan.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/tm4c123gh6pm.h
+ADC_Test_5Chan.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/PLL.h
+ADC_Test_5Chan.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/LaunchPad.h
+ADC_Test_5Chan.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/Unified_Port_Init.h
+ADC_Test_5Chan.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/CortexM.h
+
+../ADC_Test_5Chan.c:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/tm4c123gh6pm.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/PLL.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/LaunchPad.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/Unified_Port_Init.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/CortexM.h:
+
---
+++ tm4c123/src/GripSim/Debug/LaunchPad.d
@@ -0,0 +1,48 @@
+# FIXED
+
+LaunchPad.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/LaunchPad.c
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+LaunchPad.obj: /Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+LaunchPad.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/LaunchPad.h
+LaunchPad.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/Unified_Port_Init.h
+LaunchPad.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/tm4c123gh6pm.h
+LaunchPad.obj: /Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/CortexM.h
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/LaunchPad.c:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+/Applications/ti/ccs1100/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/LaunchPad.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/Unified_Port_Init.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/tm4c123gh6pm.h:
+
+/Users/anuv/Documents/School/EE445L/GripSimulator/tm4c123/inc/CortexM.h:
+
